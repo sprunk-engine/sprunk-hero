c418907a56b4cd6ffa51b94b09bd43a3be528f64
eliott <eliott.jaquier@gmail.com>
2025-03-10 18:10:57 +0100

    feat: add Chart, Mode, Note, Song, and SyncTrack interfaces for game structure

---
+++ src/models/Note.ts
@@ -0,0 +1,5 @@
+export interface Note {
+    time: number;
+    fret: number;
+    duration: number;
+}
---
+++ src/models/Song.ts
@@ -0,0 +1,7 @@
+export interface Song {
+    name: string;
+    artist: string;
+    offset: number;
+    resolution: number;
+    musicStream: string;
+}
