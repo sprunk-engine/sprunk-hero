021834880e2379706a76c173c6dda25242598954
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-03-10 14:20:51 +0100

    feat: add free look camera controls and grid rendering functionality

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import {
-    Camera,
+    Camera, Color,
     GameEngineWindow,
-    GameObject,
+    GameObject, InputGameEngineComponent,
     RenderGameEngineComponent,
     SpriteRenderBehavior,
     Sprunk
 } from "sprunk-engine";
+import {FreeLookCameraController} from "./debug/FreeLookCameraController.ts";
+import {FreeLookCameraKeyboardMouseInput} from "./debug/FreeLookCameraKeyboardMouseInput.ts";
+import {GridRenderBehavior} from "./debug/GridRenderBehavior.ts";
 
 const canvas: HTMLCanvasElement =
     document.querySelector<HTMLCanvasElement>("#app")!;
@@ -16,6 +19,8 @@ const gameEngineWindow: GameEngineWindow = Sprunk.newGame(canvas, true, [
 ]);
 const renderComponent: RenderGameEngineComponent =
     gameEngineWindow.getEngineComponent(RenderGameEngineComponent)!;
+const inputComponent: InputGameEngineComponent =
+    gameEngineWindow.getEngineComponent(InputGameEngineComponent)!;
 
 const go = new GameObject("Sprite");
 gameEngineWindow.root.addChild(go);
@@ -25,6 +30,16 @@ go.addBehavior(
 );
 
 const cameraGo = new GameObject("Camera");
+cameraGo.addBehavior(new FreeLookCameraController());
+cameraGo.addBehavior(new FreeLookCameraKeyboardMouseInput(inputComponent));
 cameraGo.addBehavior(new Camera(renderComponent, Math.PI / 2));
 cameraGo.transform.position.z = 10;
 gameEngineWindow.root.addChild(cameraGo);
+
+const grid = new GameObject("Grid");
+grid.addBehavior(
+    new GridRenderBehavior(renderComponent, 200, 1, new Color(0.3, 0.3, 0.6)),
+);
+grid.transform.rotation.setFromEulerAngles(Math.PI / 2, 0, 0);
+
+gameEngineWindow.root.addChild(grid);
