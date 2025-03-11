eaa277009d88183b0bc8549848cbb99c59372b8d
Minus <pw07vck@eduvaud.ch>
2025-03-11 16:24:44 +0100

    refactor: remove SongPlayerLogicBehavior and ChartParser usage from GameLogicGameObject

--- src/gameobjects/GameLogicGameObject.ts
+++ src/gameobjects/GameLogicGameObject.ts
@@ -1,9 +1,7 @@
 import {GameObject, InputGameEngineComponent, RenderGameEngineComponent} from "sprunk-engine";
 import {RoadGameObject} from "./RoadGameObject.ts";
 import {NotesManagerLogicBehavior} from "../behaviors/notes/NotesManagerLogicBehavior.ts";
-import {SongPlayerLogicBehavior} from "../behaviors/notes/SongPlayerLogicBehavior.ts";
 import {FretHandleGameObject} from "./FretHandleGameObject.ts";
-import {ChartParser} from "../services/chart/ChartParser.ts";
 
 /**
  * A GameObject that hold ann the movables components + game logic components of the scene (exluding camera, effects and background)
@@ -18,13 +16,8 @@ export class GameLogicGameObject extends GameObject{
         const road = new RoadGameObject(renderEngine);
         this.addChild(road);
 
-        const sontPlayBack = new SongPlayerLogicBehavior();
-        this.addBehavior(sontPlayBack);
         const noteManagter = new NotesManagerLogicBehavior(renderEngine, fretsLane.fretLogicBehaviors);
         this.addBehavior(noteManagter);
 
-        const chart = new ChartParser().parse("/assets/songs/BillyTalen-RedFlag/notes.chart");
-        console.log(chart)
-        noteManagter.setChart(chart, chart.modes[0]);
     }
 }
