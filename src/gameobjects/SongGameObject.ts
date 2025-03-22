fcc764d49ef51beb87e2853c7714086fc092ff01
eliott <eliott.jaquier@gmail.com>
2025-03-22 15:13:06 +0100

    refactor: store songId in a private variable and update album button click handler

--- src/gameobjects/SongGameObject.ts
+++ src/gameobjects/SongGameObject.ts
@@ -14,6 +14,8 @@ export class SongGameObject extends GameObject {
    */
   public onSelected: Event<string> = new Event<string>();
 
+  private _songId: string;
+
   /**
    * Creates a new SongGameObject.
    */
@@ -21,19 +23,24 @@ export class SongGameObject extends GameObject {
     songId: string
   ) {
     super(`Song_${songId}`);
+    this._songId = songId;
+  }
+
+  protected onEnable() {
+    super.onEnable();
 
     // Create album cover button
     const albumButton = new ButtonGameObject(
-      `/assets/songs/${songId}/album.png`,
-      new Vector2(1, 1),
-      2,
-      2.5,
-      1
+        `/assets/songs/${this._songId}/album.png`,
+        new Vector2(1, 1),
+        2,
+        2.5,
+        1
     );
 
     // Set click handler to emit selection event
     albumButton.onClicked.addObserver(() => {
-      this.onSelected.emit(songId);
+      this.onSelected.emit(this._songId);
     });
 
     this.addChild(albumButton);
